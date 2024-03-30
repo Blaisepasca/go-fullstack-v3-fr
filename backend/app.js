5f027320740ff9dc798458ca1ba283d68f6cd3c4
Blaise <blaiseaganzepascal10@gmail.com>
2024-03-30 06:20:07 -0700

    Let's make some improvements to our server.js file, to make it more stable and suitable for deployment

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
 
-app.use((req,res)=>{
-    res.json({message:'votre est bien a ete bien recu'});
+
+app.use((req,res,next)=>{
+    console.log('requette recu');
+    next()
 });
+app.use((req,res,next) =>{
+    res.status(201)
+    next()
+})
+app.use((req,res,next)=>{
+    res.json({ message:'votre est bien a ete bien recu'});
+    next()
+});
+app.use((req,res)=>{
+    console.log('la requette a ete bien envoye')
+})
 
 module.exports = app; 
